a0ff401a0bec1c55214f4c489f87c6a9fe344bef
Tiago Felipe <tiagosantos0412@gmail.com>
2023-11-27 10:43:21 -0300

    Primeiro commmit, criando arquivo de introdução do curso.

---
+++ Introdução/introducao.R
@@ -0,0 +1,26 @@
+# Bibliotecas #
+library(forecast)
+
+# Atribuição de variáveis #
+variavel <-  sqrt(4)
+sqrt(20) -> variavel2
+
+
+log(10)
+
+# Variáveis com operações matemáticas #
+soma <- 3 + 8
+subtracao <- 8 - 3
+multiplicacao <- 5 * 8
+divisao <- 8 / 2
+
+# Funções trigonométricas (seno, cosseno e tangente) #
+# seno #
+sin(10)
+# cosseno #
+cos(10)
+# tangente #
+tan(10)
+
+# removendo variáveis #
+rm(soma, subtracao, multiplicacao, divisao)
